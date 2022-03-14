bc790433936f7746a2835c99bb19aa888d060c8d
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2022-03-14 23:00:10 +0100

    re design onboarding

--- lib/presentation/feature/landing_screen/view/landing_view.dart
+++ lib/presentation/feature/landing_screen/view/landing_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:portfolio_app/presentation/feature/landing_screen/view/on_boarding/on_boarding_first_view.dart';
 import 'package:portfolio_app/presentation/feature/landing_screen/view/on_boarding/on_boarding_second_view.dart';
 import 'package:portfolio_app/presentation/feature/landing_screen/view/on_boarding/on_boarding_third_view.dart';
@@ -12,88 +13,19 @@ class LandingView extends StatefulWidget {
 }
 
 class _LandingViewState extends State<LandingView> {
-  final int _numPages = 3;
-  final PageController _pageController = PageController(initialPage: 0);
-  int _currentPage = 0;
-
-  List<Widget> _buildPageIndicator() {
-    List<Widget> list = [];
-    for (int i = 0; i < _numPages; i++) {
-      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
-    }
-    return list;
-  }
-
-  Widget _indicator(bool isActive) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 150),
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      height: 8.0,
-      width: isActive ? 24.0 : 16.0,
-      decoration: BoxDecoration(
-        color: isActive ? Colors.white : Color(0xFF7B51D3),
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0.1, 0.4, 0.7, 0.9],
-            colors: [
-              Color(0xFF3594DD),
-              Color(0xFF4563DB),
-              Color(0xFF5036D5),
-              Color(0xFF5B16D0),
-            ],
-          ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 40.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: PageView(
-                  physics: ClampingScrollPhysics(),
-                  controller: _pageController,
-                  onPageChanged: (int page) {
-                    setState(() {
-                      _currentPage = page;
-                    });
-                  },
-                  children: const [
-                    OnBoardingFirstView(),
-                    OnBoardingSecondView(),
-                    OnBoardingThirdView()
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: _buildPageIndicator(),
-                ),
-              ),
-              Flexible(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CtaStart(),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      body: LiquidSwipe(
+          enableLoop: true,
+          fullTransitionValue: 300,
+          waveType: WaveType.liquidReveal,
+          positionSlideIcon: 0.5,
+          pages: const [
+            OnBoardingFirstView(),
+            OnBoardingSecondView(),
+            OnBoardingThirdView(),
+          ]),
     );
   }
 }
--- lib/presentation/feature/landing_screen/view/on_boarding/on_boarding_second_view.dart
+++ lib/presentation/feature/landing_screen/view/on_boarding/on_boarding_second_view.dart
@@ -1,42 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_app/presentation/feature/landing_screen/widget/cta_start.dart';
 
 class OnBoardingSecondView extends StatelessWidget {
   const OnBoardingSecondView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Image(
-                    image: AssetImage(
-                      'assets/images/onboarding1.png',
-                    ),
-                    height: 300.0,
-                    width: 300.0,
-                  )
-                ]),
-          ),
-          Flexible(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          //stops: [0.1, 0.4, 0.7, 0.9],
+          colors: [
+            Color(0xFF8A2387),
+            Color(0xFFE94057),
+            Color(0xFFf7797d),
+          ],
+        ),
+      ),
+      child: Stack(
+        children: [
+          ClipPath(clipper: MyPainter(),)
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 40.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Text(
-                  'Connect people\naround the world',
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Image(
+                          image: AssetImage(
+                            'assets/images/onboarding1.png',
+                          ),
+                          height: 300.0,
+                          width: 300.0,
+                        )
+                      ]),
                 ),
-                Text(
-                  'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                ),
+                Flexible(
+                    child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Text(
+                        'Connect people\naround the world',
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
+                      ),
+                    ],
+                  ),
+                ))
               ],
             ),
-          ))
+          ),
         ],
       ),
     );
