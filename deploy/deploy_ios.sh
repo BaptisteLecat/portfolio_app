60771ade9b0f9cd7fcf8a6baac3aa08b2ff5d061
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-12-28 00:40:37 +0100

    add deploy files

---
+++ deploy/deploy_ios.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+NUMVERSION=$1
+NAMEVERSION=$2
+shift
+
+if [ -z "$NUMVERSION" ]; then
+    echo "Il manque le numéro de version"
+    exit 1
+else
+    echo "Version : $NUMVERSION"
+fi
+
+if [ -z "$NAMEVERSION" ]; then
+    echo "Il manque le nom de version"
+    exit 1
+else
+    echo "Nom de version : $NAMEVERSION"
+fi
+
+#flutter pub run flutter_launcher_icons:main
+CURRENT_DIR=$(pwd)
+EXPORT_PLIST=$CURRENT_DIR/export.plist
+
+flutter build ipa --export-options-plist "$EXPORT_PLIST" --release --build-number="$NUMVERSION" --build-name="$NAMEVERSION"
