8eb37ba7ae02ae166bda7fcc4f0aba2c942ffad3
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2025-02-16 21:36:40 +0100

    Fix/update project (#11)
    * Make project work again
    
    * Try to fix the workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
-import 'package:portfolio_app/firebase_options.dart';
 import 'package:portfolio_app/src/app.dart';
 import 'package:portfolio_app/src/localization/string_hardcoded.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   // turn off the # in the URLs on the web
   usePathUrlStrategy();
   // * Register error handlers. For more info, see:
@@ -23,17 +15,6 @@ Future<void> main() async {
   //registerErrorHandlers();
   // * Entry point of the app
   final container = ProviderContainer();
-  // * Register services
-  //container.read(authRepositoryProvider).signOut();
-
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
-  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
 
   runApp(
     UncontrolledProviderScope(container: container, child: const MyApp()),
